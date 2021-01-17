a0e6137a6ff806e427979caac36e11b65f98c345
Frederik Bertling <frederik.bertling@gmail.com>
2021-01-17 20:23:20 +0100

    Test commit to fix server crash

--- Framework/SwiftOSC/Communication/OSCServer.swift
+++ Framework/SwiftOSC/Communication/OSCServer.swift
@@ -96,19 +96,10 @@ open class OSCServer {
                 let length = Int(bundleData.subdata(in: Range(0...3)).toInt32())
                 let nextData = bundleData.subdata(in: 4..<length+4)
                 bundleData = bundleData.subdata(in:length+4..<bundleData.count)
-                if "#bundle\0".toData() == nextData.subdata(in: Range(0...7)){//matches string #bundle
-                    if let newbundle = self.decodeBundle(nextData){
-                        bundle?.add(newbundle)
-                    } else {
-                        bundle = nil
-                    }
+                if let message = self.decodeMessage(nextData) {
+                    bundle?.add(message)
                 } else {
-                    
-                    if let message = self.decodeMessage(nextData) {
-                        bundle?.add(message)
-                    } else {
-                        bundle = nil
-                    }
+                    bundle = nil
                 }
             }
         }
